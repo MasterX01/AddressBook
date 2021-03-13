ab8e0920fdedf33b7b7a00d82e9611487e313c5b
MasterX01 <aakashsaxena14@gmail.com>
2021-03-13 15:29:48 +0530

    [Akash] ADD. added UC-2 working code

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -4,31 +4,38 @@ public class AddressBookMain {
     Scanner scan = new Scanner(System.in);
     public void addContact() {
         ArrayList<String> addNew = new ArrayList<String>();
-        System.out.println("Please enter the First Name: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter the Last Name: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter your address: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter the City of your address: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter the State of your address: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter the ZIP of your address: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter your Phone Number: ");
-        addNew.add(scan.nextLine());
-        System.out.println("Please enter your email address: ");
-        addNew.add(scan.nextLine());
+        System.out.print("Please enter the First Name: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter the Last Name: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter your address: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter the City of your address: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter the State of your address: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter the ZIP of your address: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter your Phone Number: ");
+        addNew.add(scan.next());
+        System.out.print("Please enter your email address: ");
+        addNew.add(scan.next());
 
         //Adding contact to main list
         contact.add(addNew);
-        
-        System.out.println("The contact has been added to the Address Book");
-        for(int i = 0; i < contact.size(); i++)
-            for(int j = 0; j < contact.get(i).size(); j++)
-                System.out.print(contact.get(i).get(j) + " ");
 
+        System.out.println("The contact has been added to the Address Book");
+        for(int i = 0; i < contact.size(); i++) {
+            for (int j = 0; j < contact.get(i).size(); j++) {
+                System.out.print(contact.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Do you want to add another contact to the Address Book? (Y/N)");
+        String choice = scan.next();
+        if(choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes"))
+            addContact();
         }
 
     public static void main(String[] args) {
